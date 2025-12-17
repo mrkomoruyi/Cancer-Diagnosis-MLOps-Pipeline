90c48024655847eec0b4bb83b3771e0f31393b85
Kelvin <kelvin.omoruyi.dev@gmail.com>
2025-12-17 22:53:28 +0100

    setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+def get_requirements(file_path) -> list[str]:
+    with open(file_path, 'r') as file:
+        requirements = file.readlines()
+    return [req.strip() for req in requirements if req.strip() and not req.startswith(('#', '-e'))]
+
+setup(
+    name='mlops',
+    version='0.0.1',
+    author='Kelvin Omoruyi',
+    author_email='kelvin.omoruyi.dev@gmai.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
